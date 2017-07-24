049b50689088aff9a23fb6b662e10fe89f2fbda3
Sam Frederiksen <sam_frederiksen@y7mail.com>
2017-07-24 16:31:31 +1000

    memory access to show name

--- Program3/main.go
+++ Program3/main.go
@@ -1,4 +1,14 @@
 package main
-func main() {
 
+import "fmt"
+
+func zero(z *string) {
+	fmt.Println(z)
+	*z = "sam"
+}
+
+func main() {
+	name := "bill"
+	zero(&name)
+	fmt.Println("Hello", name)
 }
