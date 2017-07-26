338d6b8e601cd5e987bbe9e7be94ebe4cec39f24
Sam Frederiksen <sam_frederiksen@y7mail.com>
2017-07-26 15:38:13 +1000

    no errors showing in editor about to do first test

--- DiceCasino/Main.go
+++ DiceCasino/Main.go
@@ -3,7 +3,7 @@ import "fmt"
 import "time"
 import "math/rand"
 func main () {
-	//Declair global varibles
+	//Declare global varibles
 	var chance float64 = 12.38
 	var balance float64
 	var minbet float64 = balance / 2500000000
@@ -13,53 +13,70 @@ func main () {
 	var nl int64 = 0
 	var echance int64 = 0
 	var minbalance float64 = balance * .65
+	var win int64
+	var roll float64
 	//error check make sure next bet is possible
 	if  nextbet < .00000001 {
 		nextbet = .00000001
 	}
+	//reset seed
+	rand.Seed(time.Now().UnixNano())
+	//Print User settings
+	fmt.Println("Betting at 12.38% - Roll Over =",bethigh)
 	//Bets start here
 	for {
-		rand.Seed(time.Now().UnixNano())
+		// Determine if bet is a win or loss
+		roll = rand.Float64()
+		if roll < chance {
+			win = 1
+			} else {
+			win = 0
+			}
+		// Print Stats
 		ld = ld + 1
 		fmt.Println("Last Double Win")
 		fmt.Println(ld)
 		fmt.Println("rolls ago")
 		fmt.Println("Number of Loss's in a Row")
 		fmt.Println(nl)
-		if win | echance > 0 {
+
+		// If We already had more than 2 wins in a row
+		if win == 1 && echance > 0 {
 			nextbet = nextbet * 3.35
 			echance = echance + 1
 			nl = 0
 			ld = 0
 		}
-		if win | echance == 0 {
+		// If this is 2nd win in a row
+		if win == 1 && echance == 0 {
 			nextbet = nextbet * .5
 			echance = echance + 1
 			minbalance = balance * .65
 			nl = 0
 		}
-		if !win | echance > 0 {
+		// If not win after more than 1 win
+		if win == 0 && echance > 0 {
 			nextbet = minbet
 			echance = 0
 			nl = nl + 1
 		}
-		if !win | echance == 0 {
+		// if first win after 1 or more loss's
+		if win == 0 && echance == 0 {
 		nextbet = nextbet * (1.235)
 		nl = nl + 1
 		echance = 0
 		}
-		if ld <= 50000 {
+		//setup new minbet
 		minbet = balance / 2500000000
-		}
-		if ld > 50000 {
-		minbet = balance / 9000000000
-		}
+		//errorcheck to see if minbet is possible
 		if minbet < .00000001 {
 			minbet = .00000001
 		}
+		//error check to see if nextbet is possible
 		if nextbet < minbet {
 			nextbet = minbet
 		}
+		//check to see if we hit min balance or not
 		if balance < minbalance {
 			fmt.Println("Really Bad Luck Better Luck Next Time")
 			break
