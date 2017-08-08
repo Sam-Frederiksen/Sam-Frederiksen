ce0e050b0502c1c25458b2757cad53620385e9f3
Sam Frederiksen <sam_frederiksen@y7mail.com>
2017-08-09 00:30:40 +1000

    working on 10

---
+++ projecteulerproblem10/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		i   int
+		j   int
+		t   int
+		sum int
+	)
+	for i = 1; i < 2000000; i++ {
+		t = 0
+		for j = 2; j < i/2; j++ {
+			if i%j == 0 {
+				t = 1
+				break
+			}
+		}
+		if t != 1 {
+			sum = sum + i
+		}
+	}
+	fmt.Println(sum)
+}
--- projecteulerproblem9/main.go
+++ projecteulerproblem9/main.go
@@ -1,8 +1,30 @@
 package main
+
+import "fmt"
+
 func main() {
 	var (
-
+		a int
+		b int
+		c int
+		d int
+		f int
 	)
-
-
+	fmt.Println(a, b, c, d)
+	for   {
+		for a=1;a<500;a++{
+			for b=1;b<500;b++{
+				for c=1;c<500;c++{
+					if a+b+c ==1000 && (a*a + b*b)==c*c{
+						d=a*b*c
+						f=1
+					}
+				}
+			}
+		}
+		if f ==1 {
+			fmt.Println(d)
+			break
+		}
+	}
 }
